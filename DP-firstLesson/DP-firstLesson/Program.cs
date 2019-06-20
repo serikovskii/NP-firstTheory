1fdab1df7b24644544a92002598628b48613f931
Azat <46015824+serikovskii@users.noreply.github.com>
2019-06-20 11:59:19 +0600

    update

--- DP-firstLesson/DP-firstLesson/Program.cs
+++ DP-firstLesson/DP-firstLesson/Program.cs
@@ -12,50 +12,54 @@ namespace DP_firstLesson
     {
         static void Main(string[] args)
         {
-            string compName = Dns.GetHostName();
-            Console.WriteLine($"This hosy name: {compName}");
-            IPHostEntry ipList = Dns.GetHostByName(compName);
+            //string compName = Dns.GetHostName();
+            //Console.WriteLine($"This hosy name: {compName}");
+            //IPHostEntry ipList = Dns.GetHostByName(compName);
 
-            foreach (var ip in ipList.AddressList)
-            {
-                Console.WriteLine(ip.ToString());
-            }
+            //foreach (var ip in ipList.AddressList)
+            //{
+            //    Console.WriteLine(ip.ToString());
+            //}
 
-            compName = Console.ReadLine();
+            //compName = Console.ReadLine();
 
-            ipList = Dns.GetHostByName(compName);
+            //ipList = Dns.GetHostByName(compName);
 
-            foreach (var ip in ipList.AddressList)
-            {
-                Console.WriteLine(ip.ToString());
-            }
-            Console.ReadLine();
+            //foreach (var ip in ipList.AddressList)
+            //{
+            //    Console.WriteLine(ip.ToString());
+            //}
+            //Console.ReadLine();
 
-            Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            //Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
 
-            socket.Connect(ipList.AddressList[0], 443);
-            try
-            {
-                if (socket.Connected)
-                {
-                    string httpRequest = "GET/HTTP1.0";
-                    socket.Send(Encoding.ASCII.GetBytes(httpRequest));
-                    var buffer = new byte[1024 * 4];
-                    socket.Receive(buffer);
+            //socket.Connect(ipList.AddressList[0], 443);
+            //try
+            //{
+            //    if (socket.Connected)
+            //    {
+            //        string httpRequest = "GET/HTTP1.0";
+            //        socket.Send(Encoding.ASCII.GetBytes(httpRequest));
+            //        var buffer = new byte[1024 * 4];
+            //        socket.Receive(buffer);
 
-                    Console.WriteLine($" Recived: {Encoding.UTF8.GetString(buffer)}");
-                }
-                else
-                {
-                    Console.WriteLine("Error");
+            //        Console.WriteLine($" Recived: {Encoding.UTF8.GetString(buffer)}");
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine("Error");
 
-                }
+            //    }
 
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine("ERROR: " + ex.Message);
-            }
+            //}
+            //catch (Exception ex)
+            //{
+            //    Console.WriteLine("ERROR: " + ex.Message);
+            //}
+
+            IPAddress ip = IPAddress.Parse("0.0.0.0");
+            var port = 12345;
+            Server(ip, port); 
         }
 
         static void Server(IPAddress ipAddress, int port)
@@ -64,10 +68,25 @@ namespace DP_firstLesson
             IPEndPoint serverEndPoint = new IPEndPoint(ipAddress, port);
             serverSocket.Bind(serverEndPoint);
             serverSocket.Listen(100);
-            Socket clientSocket = serverSocket.Accept();
+            while (true)
+            {
+                Socket clientSocket = serverSocket.Accept();
+                Console.WriteLine("Client connected: ", clientSocket.RemoteEndPoint.ToString());
+                try
+                {
+                    var buffer = new byte[1024];
+                    int reciveSize = clientSocket.Receive(buffer);
+                    Console.WriteLine($"Recived {reciveSize} bytes");
+                    Console.WriteLine(Encoding.UTF8.GetString(buffer));
+                    var sendSize = clientSocket.Send(buffer, reciveSize, SocketFlags.None);
+                    Console.WriteLine($"Send to client {sendSize} bytes");
 
-            clientSocket.Send();
-            clientSocket.Receive();
+                }
+                catch(Exception exception)
+                {
+                    Console.WriteLine("Error: " + exception.Message);
+                }
+            }
         }
     }
 }
